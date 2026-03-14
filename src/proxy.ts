afec664b8f9a77206625b09bdc675d5b50a8e1f1
Shadowtrooper11 <markgarcia414@gmail.com>
2026-03-13 20:37:26 -0500

    Changed middleware to proxy to remove depreciation of middleware warning.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
-import createMiddleware from 'next-intl/middleware';
+import createProxy from 'next-intl/middleware';
 import {routing} from './i18n/navigation';
  
-export default createMiddleware(routing);
+export default createProxy(routing);
  
 export const config = {
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
